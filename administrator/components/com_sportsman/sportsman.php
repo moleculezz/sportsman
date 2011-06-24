4424a5b24d333124694cb45bfdcac5b9f012525c
G. Arends <g.inc@gdarends.com>
2011-06-24 20:54:27 +0200

    add component main class

---
+++ administrator/components/com_sportsman/sportsman.php
@@ -0,0 +1,9 @@
+<?php
+
+// Check if Koowa is active
+if(!defined('KOOWA')) {
+    JError::raiseWarning(0, JText::_("Koowa wasn't found. Please install the Koowa plugin and enable it."));
+    return;
+}
+
+echo KFactory::get('admin::com.sportsman.dispatcher')->dispatch();
