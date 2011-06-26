f985a89f74ff73fbd261bce07f6d0204979a4043
G. Arends <g.inc@gdarends.com>
2011-06-26 19:37:40 +0200

    Added dashboard controller and dispatcher

---
+++ administrator/components/com_sportsman/dispatcher.php
@@ -0,0 +1,13 @@
+<?php
+
+class ComSportsmanDispatcher extends ComDefaultDispatcher
+{
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'controller' => 'dashboard'
+        ));
+
+        parent::_initialize($config);
+    }
+}
