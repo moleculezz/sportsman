14c6d1ec59b34a3b96b686df6b87c871e8cb8ce4
G. Arends <g.inc@gdarends.com>
2011-07-08 19:58:01 +0200

    Added menubar

---
+++ administrator/components/com_sportsman/controllers/toolbars/menubar.php
@@ -0,0 +1,26 @@
+<?php
+
+class ComSportsmanControllerToolbarMenubar extends ComDefaultControllerToolbarMenubar
+{
+    public function getCommands()
+    { 
+        $name = $this->getController()->getIdentifier()->name;
+        
+        $this->addCommand('Dashboard', array(
+        	'href'   => JRoute::_('index.php?option=com_sportsman'),
+        	'active' => ($name == 'dashboard')
+        ));
+        
+        $this->addCommand('Divisions', array(
+        	'href'   => JRoute::_('index.php?option=com_sportsman&view=divisions'),
+        	'active' => ($name == 'division')
+        ));
+        
+        $this->addCommand('Sports', array(
+        	'href'   => JRoute::_('index.php?option=com_sportsman&view=sports'),
+        	'active' => ($name == 'sport')
+        ));
+        
+        return parent::getCommands();
+    }
+}
