05ae43a5851952f7a9821a6f4a8e050a4f7b8749
G. Arends <g.inc@gdarends.com>
2011-07-24 14:48:24 +0200

    Added sports listbox helper

---
+++ code/admin/components/com_sportsman/templates/helpers/listbox.php
@@ -0,0 +1,18 @@
+<?php
+
+class ComSportsmanTemplateHelperListbox extends ComDefaultTemplateHelperListbox
+{
+    public function sports( $config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'model'    => 'sports',
+            'name'     => 'sportsman_sport_id',
+            'value'    => 'id',
+            'text'     => 'title',
+            'prompt'   => '- Select Sport -',
+            'attribs'  => array('id' => $config->name)
+        ));
+        return parent::_listbox($config);
+    }
+}
