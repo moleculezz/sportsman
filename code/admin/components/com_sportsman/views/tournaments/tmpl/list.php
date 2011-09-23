c71109e1fb1ecfd0c03c413e19692a07ee12da27
G. Arends <gdarends@gmail.com>
2011-09-23 13:09:32 +0200

    Add games sidebar tournament list

---
+++ code/admin/components/com_sportsman/views/tournaments/tmpl/list.php
@@ -0,0 +1,18 @@
+<?php 
+
+defined('KOOWA') or die( 'Restricted access' ); ?>
+
+<ul>
+	<li class="<?= $state->tournament == null ? 'active' : ''; ?>">
+		<a href="<?= @route('tournament=' ) ?>">
+			<?= 'All Tournaments' ?>
+		</a>
+	</li>
+	<? foreach ($tournaments as $tournament) : ?>
+	<li class="<?= $state->tournament == $tournament->id ? 'active' : ''; ?>">
+		<a href="<?= @route('tournament='.$tournament->id ) ?>">
+			<?= @escape($tournament->title) ?>
+		</a>
+	</li>
+	<? endforeach ?>
+</ul>
