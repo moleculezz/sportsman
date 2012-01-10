8a7fcfda63f83730cd4ee66ab92a0cfaf0cd0e7d
G. Arends <gdarends@gmail.com>
2012-01-10 22:46:41 +0100

    Add venues model

---
+++ code/admin/components/com_sportsman/databases/tables/venues.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @category	Nooku
+ * @package		Sportsman
+ * @copyright	Copyright (C) G.D. Arends. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ */
+ 
+ 
+ /**
+ * Venues Database Table Class
+ *
+ * @author      G.D. Arends <https://github.com/moleculezz>
+ * @category    Nooku
+ * @package     Sportsman 
+ */
+class ComSportsmanDatabaseTableVenues extends KDatabaseTableAbstract
+{
+    public function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'name'       => 'sportsman_view_venues',
+            'base'       => 'sportsman_venues',
+            'behaviors'  => array('creatable', 'modifiable', 'identifiable')
+        ));
+
+        parent::_initialize($config);
+    }
+}
---
+++ code/admin/components/com_sportsman/models/venues.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @category	Nooku
+ * @package		Sportsman
+ * @copyright	Copyright (C) G.D. Arends. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ */
+ 
+ 
+ /**
+ * Venues Model Class
+ *
+ * @author      G.D. Arends <https://github.com/moleculezz>
+ * @category    Nooku
+ * @package     Sportsman 
+ */
+class ComSportsmanModelVenues extends ComDefaultModelDefault
+{
+    public function __construct(KConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_state
+            ->insert('club'   , 'int');
+    }
+    
+    protected function _buildQueryWhere(KDatabaseQuery $query)
+    {
+        $state = $this->_state;
+        
+        if (is_numeric($state->club)) {
+            $query->where('tbl.sportsman_club_id', '=', $state->club);
+        }
+        
+        parent::_buildQueryWhere($query);
+    }
+}
